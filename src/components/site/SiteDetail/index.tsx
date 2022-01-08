21d6357157e88cdba3a8b162cf0aef10a0afd4d6
kobiyoyo <adamachubi@gmail.com>
2022-01-08 22:24:25 +0100

    feat(site): add site detail and form components

--- src/components/site/SiteDetail/index.tsx
+++ src/components/site/SiteDetail/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Button, Col, Row } from 'antd';
+import MetricsList from '../../metrics/MetricsList';
+import CategoryList from '../../category/CategoryList';
 
-const SiteDetail = () => {
-  const hello = 'Detail';
-  return (
-    <div>
-      {hello}
-    </div>
-  );
-};
+const SiteDetail = () => (
+  <div>
+    <Button> Add Metrics</Button>
+    <Row>
+      <Col xs={18} sm={18} md={18} lg={18} xl={18}>
+        <MetricsList />
+      </Col>
+      <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+        <CategoryList />
+      </Col>
+    </Row>
+  </div>
+);
 
 export default SiteDetail;
--- src/components/site/SiteForm/index.tsx
+++ src/components/site/SiteForm/index.tsx
@@ -4,7 +4,7 @@ import {
 } from 'antd';
 import { Formik, FormikHelpers } from 'formik';
 import {
-  Form, FormikDebug, FormItem, ResetButton, SubmitButton, Input,
+  Form, FormItem, ResetButton, SubmitButton,
 } from 'formik-antd';
 import InputFormField from '../../../utils/input/Input';
 import { SiteFormValidation } from '../schema/SiteFormValidation';
@@ -12,7 +12,7 @@ import { initialValues } from '../schema/SiteDefaultValues';
 import { SiteFormValues } from '../types';
 
 const SiteForm = () => {
-  const handleEditAccount = (
+  const onSubmit = (
     values: SiteFormValues,
     { setSubmitting }: FormikHelpers<SiteFormValues>,
   ) => {
@@ -27,7 +27,7 @@ const SiteForm = () => {
       <Col xs={24} sm={24} md={24} lg={24} xl={24}>
         <Formik
           initialValues={initialValues}
-          onSubmit={handleEditAccount}
+          onSubmit={onSubmit}
           validationSchema={SiteFormValidation}
           render={(formikProps) => (
             <Form layout="vertical">
@@ -35,13 +35,11 @@ const SiteForm = () => {
                 <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                   <Row justify="center">
                     <Col xs={7} sm={7} md={7} lg={7} xl={7}>
-                      <FormItem name="Name">
-                        <Input
-                          name="name"
-                          size="middle"
-                          placeholder="Name"
-                        />
-                      </FormItem>
+                      <InputFormField
+                        name="name"
+                        size="middle"
+                        placeholder="Name"
+                      />
                       {formikProps.touched.name
                       && formikProps.errors.name && <Text className="text-red-600">{formikProps.errors.name}</Text>}
                     </Col>
