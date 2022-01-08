849227e4ab0e63f6ca6d3256ab60048360ebb7c1
kobiyoyo <adamachubi@gmail.com>
2022-01-08 22:27:01 +0100

    feat(category): add category components

---
+++ src/components/category/CategoryForm/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Col, Row, Typography,
+} from 'antd';
+import { Formik, FormikHelpers } from 'formik';
+import {
+  Form, FormikDebug, FormItem, ResetButton, SubmitButton, Input,
+} from 'formik-antd';
+import InputFormField from '../../../utils/input/Input';
+import { CategoryFormValidation } from '../schema/CategoryFormValidation';
+import { initialValues } from '../schema/CategoryDefaultValues';
+import { CategoryFormValues } from '../types';
+
+const CategoryForm = ({ siteId }: { siteId: number}) => {
+  const id = siteId;
+  const { Text } = Typography;
+
+  const onSubmit = (
+    values: CategoryFormValues,
+    { setSubmitting }: FormikHelpers<CategoryFormValues>,
+  ) => {
+    console.log(values);
+    setSubmitting(false);
+  };
+
+  return (
+    <Row>
+      <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={CategoryFormValidation}
+          render={(formikProps) => (
+            <Form layout="vertical">
+              <Row gutter={[0, 20]}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                  <Row justify="center">
+                    <Col xs={7} sm={7} md={7} lg={7} xl={7}>
+                      <InputFormField
+                        name="name"
+                        size="middle"
+                        placeholder="Name"
+                      />
+                      {formikProps.touched.name
+                      && formikProps.errors.name && <Text className="text-red-600">{formikProps.errors.name}</Text>}
+                    </Col>
+                  </Row>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                  <Row justify="center">
+                    <Col xs={7} sm={7} md={7} lg={7} xl={7}>
+                      <FormItem name="button">
+                        <SubmitButton size="large" className="bg-blue-900">Save</SubmitButton>
+                        <ResetButton className="mx-4" size="large">
+                          Cancel
+                        </ResetButton>
+                      </FormItem>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Form>
+          )}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default CategoryForm;
---
+++ src/components/category/CategoryList/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Button, Col, Input, Row,
+} from 'antd';
+import Card from '../../../utils/card/Card';
+import Title from '../../../utils/title/Title';
+import CategoryForm from '../CategoryForm';
+
+const CategoryList = () => (
+  <Row>
+    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+      <Title title="Category" />
+      <CategoryForm siteId={1} />
+      <Row justify="center">
+        <Col md={7} xl={7}>
+          <Row gutter={[0, 50]}>
+            <Card size={24} title="Hello" id={0} />
+            <Card size={24} title="You" id={0} />
+            <Card size={24} title="Bad" id={0} />
+          </Row>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+);
+
+export default CategoryList;
