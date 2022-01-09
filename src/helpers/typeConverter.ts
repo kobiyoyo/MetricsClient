5f87220c1a963bb37a21bd678c2a06dec26c55f0
kobiyoyo <adamachubi@gmail.com>
2022-01-09 18:32:28 +0100

    feat(type): create types helper for converting objects type

--- src/helpers/typeConverter.ts
+++ src/helpers/typeConverter.ts
@@ -4,5 +4,10 @@ export type CamelToSnakeCase<S extends string> =
   `${T extends Capitalize<T> ? '_' : ''}${Lowercase<T>}${CamelToSnakeCase<U>}` :
   S
 
-// Convert type from camel case to snake case
+// Convert type object from camel case to snake case
 export type CamelToSnakeCaseObject <T> = T extends object ? { [K in keyof T as CamelToSnakeCase<K & string>]: CamelToSnakeCaseObject<T[K]>}: T
+
+// Convert type nested object from camel case to snake case
+export type CamelToSnakeCaseNested<T> = T extends object ? {
+  [K in keyof T as CamelToSnakeCase<K & string>]: CamelToSnakeCaseNested<T[K]>
+} : T
